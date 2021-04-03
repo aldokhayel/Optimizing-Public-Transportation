fc9d2d86743f42ca9e45da86e5e3af5a6b360831
aldokhayel <abdulrahman@Abdulrahmans-MacBook-Pro.local>
2021-04-03 11:30:43 +0300

    fixing issues

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -39,7 +39,7 @@ class Turnstile(Producer):
         #
         topic_name="oturnstile",
         super().__init__(
-            topic_name=,
+            topic_name,
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=3,
